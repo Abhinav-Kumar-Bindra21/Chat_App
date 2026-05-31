0204657452d9b78dbfe1006891b50c90fa4df996
Abhinav kumar Bindra <abhinavbindra1258@gmail.com>
2026-05-31 15:32:13 +0530

    update profile endpoint added

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import { signup, login, logout, updateProfile, getUser } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/signup", signup);
 authRouter.post("/login", login);
 authRouter.post("/logout", logout);
+authRouter.put("/update-profile", protectRoute, updateProfile);
+
+authRouter.get("/check", protectRoute, getUser);
 
 export default authRouter;
