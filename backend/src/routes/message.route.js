6339e9aade4d33b6c2ed62dd3bd311b6ed44b50c
Abhinav kumar Bindra <abhinavbindra1258@gmail.com>
2026-06-01 13:56:56 +0530

    message route added

---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { getAllContacts, getMessageByUserId, sendMessage, getChatPartners } from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
+
+const messageRouter = express.Router();
+
+messageRouter.use(arcjetProtection, protectRoute);
+
+messageRouter.get("/contacts", getAllContacts);
+messageRouter.get("/chats", getChatPartners);
+messageRouter.get("/:id", getMessageByUserId);
+
+messageRouter.post("/send/:id", sendMessage);
+
+export default messageRouter;
