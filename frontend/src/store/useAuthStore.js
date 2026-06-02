bf1decdd9415b17e01499a60e5e5c168032cdae4
Abhinav kumar Bindra <abhinavbindra1258@gmail.com>
2026-06-02 21:37:43 +0530

    signup implentened in frontend

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,6 +1,36 @@
 import { create } from "zustand";
+import { axiosInstance } from "../libs/axios";
+import toast from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
-  authUser: { name: "Ahinav", _id: 123, age: 12 },
-  isLoading: false,
+  authUser: null,
+  isCheckingAuth: true,
+  isSigninUp: false,
+
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+      set({ authUser: res.data });
+    } catch (error) {
+      console.log("Error in authCheck", error.message);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+
+  signup: async (data) => {
+    set({ isSigninUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({ authUser: res.data });
+
+      toast.success("Account created successfully");
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isSigninUp: false });
+    }
+  },
 }));
